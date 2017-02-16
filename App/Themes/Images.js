290329fa13a23b9339b5002acbbad929c51c15d9
jnocon <nocon.jesse@gmail.com>
2017-02-15 16:41:29 -0800

    home page completed

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -2,11 +2,12 @@
 
 // leave off @2x/@3x
 const images = {
-  logo: require('../Images/ir.png'),
+  logo: require('../Images/logoNoTextSmall.png'),
   clearLogo: require('../Images/top_logo.png'),
   ignite: require('../Images/ignite_logo.png'),
   tileBg: require('../Images/tile_bg.png'),
-  background: require('../Images/BG.png')
+  background: require('../Images/BG.png'),
+  atextaLogo: require('../Images/logo2.png')
 }
 
 export default images
